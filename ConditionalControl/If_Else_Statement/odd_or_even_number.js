5cff2bd011bd8ee49bc29f65f230c765d4da8fad
rajendraxettri <xettrijr@gmail.com>
2023-09-14 15:24:57 +0545

    Added project !

---
+++ ConditionalControl/If_Else_Statement/odd_or_even_number.js
@@ -0,0 +1,7 @@
+let num =21
+
+if(num% 2 == 0){
+    console.log("Even Number !")
+}else{
+    console.log("Odd number !")
+}
---
+++ ConditionalControl/Switch_Statement/switchCaseInJs.js
@@ -0,0 +1,37 @@
+let day = 3
+
+
+switch(day){
+
+    case 1: 
+    console.log("The day is Sunday !")
+    break;
+    
+    case 2: 
+    console.log("The day is Monday !")
+    break;
+    
+    case 3: 
+    console.log("The day is Tuesday !")
+    break;
+    
+    case 4: 
+    console.log("The day is Wednesday !")
+    break;
+    
+    case 5: 
+    console.log("The day is Thursday !")
+    break;
+    
+    case 6: 
+    console.log("The day is Friday !")
+    break;
+    
+    case 7: 
+    console.log("The day is Saturday !")
+    break;
+
+    default:{
+        console.log("Sorry Invalide Option ! ")
+    }
+}
